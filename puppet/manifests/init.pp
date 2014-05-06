82373150e880169ca5fe64c657fbb420382dd6d5
Mick Pollard <aussielunix@gmail.com>
2014-05-06 11:09:44 +1000

    add basic puppet manifests complete with tests

--- puppet/manifests/init.pp
+++ puppet/manifests/init.pp
@@ -1,7 +1,22 @@
-node default {
-  class { 'nginx': }
+node default  {
 
-  nginx::resource::vhost { 'example.org':
-    www_root => '/var/www/example.org',
+  class { 'apache':
+    default_mods        => true,
+    default_confd_files => false,
+    mpm_module          => 'prefork'
+  }
+
+  class {'::apache::mod::php': }
+
+  apache::vhost { 'example.org':
+    port          => '80',
+    docroot       => '/var/www/example.org',
+    docroot_owner => 'root',
+    docroot_group => 'vagrant',
+  }
+
+  file{'/var/www/example.org/index.php':
+    content => 'Hello World - example.org',
+    require => Apache::Vhost['example.org']
   }
 }
