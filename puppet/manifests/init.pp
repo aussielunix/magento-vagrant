c4955d5d87c8bd792eb960d1ae30ec100d40715e
Mick Pollard <aussielunix@gmail.com>
2014-05-06 16:29:07 +1000

    add thias/php module and some manifest to drive it

--- puppet/manifests/init.pp
+++ puppet/manifests/init.pp
@@ -6,8 +6,6 @@ node default  {
     mpm_module          => 'prefork'
   }
 
-  class {'::apache::mod::php': }
-
   apache::vhost { 'example.org':
     port          => '80',
     docroot       => '/var/www/example.org',
@@ -19,4 +17,45 @@ node default  {
     content => 'Hello World - example.org',
     require => Apache::Vhost['example.org']
   }
+
+  class {'::apache::mod::php': } -> 
+  php::ini { '/etc/php5/apache2/php.ini':
+    mail_add_x_header => 'Off',
+    # For the parent directory
+    require => Package['apache2'],
+  } ->
+  class { 'php::mod_php5':
+    inifile => '/etc/php5/apache2/php.ini'
+  } 
+
+
+  class { 'php::cli':
+    inifile => '/etc/php5/cli/php.ini'
+  }
+
+  php::ini { '/etc/php5/cli/php.ini':
+    display_errors => 'On',
+    memory_limit   => '64M',
+  }
+
+  php::module { 'gd': }
+  php::module { 'mcrypt': }
+  php::module { 'apc': }
+  php::module { 'curl': }
+  php::module { 'mysqlnd': }
+  php::module { 'redis':
+    require => Apt::Ppa['ppa:ufirst/php']
+  }
+  php::module { 'snappy':
+    require => Apt::Ppa['ppa:ufirst/php']
+  }
+
+  class { 'apt': }
+
+  apt::key { 'ufirst':
+    key        => '4BD6EC30',
+    key_server => 'pgp.mit.edu',
+  }
+
+  apt::ppa { 'ppa:ufirst/php': }
 }
