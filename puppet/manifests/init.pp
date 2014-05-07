f5bc3de7f937a7650dd3ccbb6dbc7a64c28a0408
Mick Pollard <aussielunix@gmail.com>
2014-05-07 20:03:46 +1000

    move node level data out to hiera

--- puppet/manifests/init.pp
+++ puppet/manifests/init.pp
@@ -1,8 +1,13 @@
 #
 node default  {
   class {'roles::web':
-    php_runtime => 'modphp',
-    webserver   => 'apache2',
-    url         => 'example.org'
+    php_runtime        => hiera('roles::web::php_runtime'),
+    webserver          => hiera('roles::web::webserver'),
+    url                => hiera('url'),
+    apc_enabled        => hiera('php::module::apc::enabled'),
+    apc_max_file_size  => hiera('php::module::apc::max_file_size'),
+    apc_shm_size       => hiera('php::module::apc::shm_size'),
+    apc_num_files_hint => hiera('php::module::apc::num_files_hint'),
+    apc_enable_cli     => hiera('php::module::apc::enable_cli')
   }
 }
