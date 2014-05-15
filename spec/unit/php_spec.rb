9b7699c61d274864864b8bf5efdc6fa6ee60da60
Mick Pollard <aussielunix@gmail.com>
2014-05-16 09:46:30 +1000

    add basic mod_php unit test

---
+++ spec/unit/php_spec.rb
@@ -0,0 +1,18 @@
+require_relative 'spec_helper'
+
+describe package('php5') do
+  it { should be_installed }
+end
+
+describe package('php5-fpm') do
+  it { should_not be_installed }
+end
+
+describe service('php-fpm') do
+  it { should_not be_running   }
+end
+
+describe file('/etc/php5/apache2/php.ini') do
+  it { should be_file }
+  its(:content) { should match /memory_limit/ }
+end
